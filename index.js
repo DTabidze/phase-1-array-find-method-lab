9b261d2b092826807f1e427e83f472cd8d274112
Davit Tabidze <davidtabidzef1@gmail.com>
2023-05-23 21:21:26 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+
+function superbowlWin(record) {
+    const found = record.find(element => element["result"] === "W");
+    if (found) {
+        return found.year;
+    } else {
+        return found;
+    }
+    //return found;
+}
